0dcf0e8d0a022aa2d10b2ce4ba8ecad042e63600
emarmorrison <emarmorrison@gmail.com>
2021-02-27 10:00:34 -0500

    getter setter and constructor

--- src/com/emar/Student.java
+++ src/com/emar/Student.java
@@ -4,4 +4,46 @@ public class Student {
     private int studentId;
     private String firstName;
     private String lastName;
+    private String city;
+
+    public Student(int studentId, String firstName, String lastName, String city) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
 }
